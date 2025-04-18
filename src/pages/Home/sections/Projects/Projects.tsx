0f9f26d55e92c9c61cc5c5930bc63c23f212d3c4
Miguel Augusto Baricelli <baricellimiguel@gmail.com>
2025-04-18 00:19:57 -0300

    Adiciona seção de cards de projetos

---
+++ src/pages/Home/sections/Projects/Projects.tsx
@@ -0,0 +1,12 @@
+import CardsProjects from "../../../../components/components-projects/CardsProjects"
+
+const Projects = () => {
+
+    return (
+        <CardsProjects />
+
+    )
+
+}  
+
+export default Projects;
